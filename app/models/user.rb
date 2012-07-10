819fb7dafe4132c5200d5c24d7c09d487618290d
Miguel Bejar <bejar37@gmail.com>
2012-07-10 14:47:25 -0400

    changed some of the devise views

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,5 +8,5 @@ class User < ActiveRecord::Base
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :name, :email, :password, :password_confirmation, :remember_me, :confirmed_at
-  
+
 end
